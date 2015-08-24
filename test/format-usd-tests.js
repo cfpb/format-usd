a53c6a883f3f9d0c5343d33d7b9f8c05077f8ff4
Chuck Werner <mistergone@gmail.com>
2015-08-24 15:26:36 -0400

    Tests for lib/* functions

--- test/format-usd-tests.js
+++ test/format-usd-tests.js
@@ -1,4 +1,6 @@
-var formatUSD = require('../index.js');
+var formatUSD = require('../index.js'),
+    commaSeparate = require('../lib/comma-separate-number.js'),
+    handleStringInput = require('../lib/handle-string-input.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -81,6 +83,55 @@ exports.formatUSD = {
     test.equal( formatUSD(-99, {decimalPlaces: 0}), '-$99' );
     test.equal( formatUSD(-1234, {decimalPlaces: 0}), '-$1,234' );
     test.equal( formatUSD(-5.55, {decimalPlaces: 2}), '-$5.55' );
+    test.done();
+  }
+};
+
+exports.handleStringInput = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'Parse number strings with non-numeric characters': function(test) {
+    test.expect(6);
+    // tests here
+    test.equal( handleStringInput( '9a99' ), 999 );
+    test.equal( handleStringInput( 'u123456' ), 123456 );
+    test.equal( handleStringInput( '01234' ), 1234 );
+    test.equal( handleStringInput( '$1,234,567' ), 1234567 );
+    test.equal( handleStringInput( 'Ilikethenumber5' ), 5 );
+    test.equal( handleStringInput( 'function somefunction() { do badstuff; }' ), 0 );
+    test.done();
+  },
+  'Parse the first period as a decimal point': function(test) {
+    test.expect(3);
+    // tests here
+    test.equal( handleStringInput( '4.22' ), 4.22 );
+    test.equal( handleStringInput( 'I.like.the.number.5' ), 0.5 );
+    test.equal( handleStringInput( '1.2.3.4.5.6.7' ), 1.234567 );
+    test.done();
+  }
+};
+
+exports.commaSeparate = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'Comma separte a numeric string': function(test) {
+    test.expect(4);
+    // tests here
+    test.equal( commaSeparate( '1234567' ), '1,234,567' );
+    test.equal( commaSeparate( '1234' ), '1,234' );
+    test.equal( commaSeparate( '5' ), '5' );
+    test.equal( commaSeparate( '199' ), '199' );
+    test.done();
+  },
+  'Handle numeric strings with >3 decimal places': function(test) {
+    test.expect(2);
+    // tests here
+    test.equal( commaSeparate( '9.1234' ), '9.1234' );
+    test.equal( commaSeparate( '12345678.999999' ), '12,345,678.999999' );
     test.done();
   }
 };
