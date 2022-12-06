bab7669ef4b6988486ce670f078555df1d9c80c0
Ans <git@anselmbradford.com>
2022-12-06 18:42:40 -0500

    Convert to ESM

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import handleStringInput from './lib/handle-string-input.js';
+import commaSeparate from './lib/comma-separate-number.js';
+
 /**
  * @param {object} opts The options object
  * @param {number|string} opts.amount The number or string to be formatted
@@ -5,42 +8,37 @@
  *   you'd like in the returned string
  * @returns {string}      The number in USD format.
  */
-
-'use strict';
-
-function formatMoney( opts ) {
-  var num = opts.amount,
-      decPlaces = 0,
-      sign = '',
-      numericValue = num,
-      stringValue = '',
-      formattedString = '',
-      handleStringInput = require('./lib/handle-string-input.js'),
-      commaSeparate = require('./lib/comma-separate-number.js');
+function formatUSD(opts) {
+  let num = opts.amount;
+  let decPlaces = 0;
+  let sign = '';
+  let numericValue = num;
+  let stringValue = '';
+  let formattedString = '';
 
   // Handle a String as input
-  if ( typeof num === 'string' ) {
-    numericValue = handleStringInput( num );
+  if (typeof num === 'string') {
+    numericValue = handleStringInput(num);
   }
 
   // Determine sign
-  if ( numericValue < 0 ) {
+  if (numericValue < 0) {
     sign = '-';
   }
 
   opts = opts || {};
 
   // Determine decimal places
-  decPlaces = Math.abs( opts.decimalPlaces );
-  if ( isNaN( decPlaces ) ) {
+  decPlaces = Math.abs(opts.decimalPlaces);
+  if (isNaN(decPlaces)) {
     decPlaces = 2;
   }
 
   // Get absolute value, apply decimal places limit, return string
-  stringValue = Math.abs( numericValue ).toFixed( decPlaces );
+  stringValue = Math.abs(numericValue).toFixed(decPlaces);
 
   // Get a comma-separated string of the absolute value of numericValue
-  stringValue = commaSeparate( stringValue );
+  stringValue = commaSeparate(stringValue);
 
   // Construct the formattedString
   formattedString = sign + '$' + stringValue;
@@ -48,4 +46,4 @@ function formatMoney( opts ) {
   return formattedString;
 }
 
-module.exports = formatMoney;
+export { formatUSD };
